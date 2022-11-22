edb29142f09ad38524d798db1c98fc538e6b16e2
Camille TOURON <camille.touron.pro@gmail.com>
2022-11-22 21:34:07 +0100

    Mise en place middleware d'erreur par defaut.

--- app.js
+++ app.js
@@ -8,5 +8,16 @@ app.use(
         apiSpec: './configurations/open_api.yaml'
     })
 );
+//Routes :
+
+//Error middleware :
+app.use((error, req, res, next) => {
+    if(error.status!=null){
+        res.status(error.status).json({message: error.message});
+    }else{
+        res.status(500).json({message: "Server error."});
+    }
+    
+});
 
 module.exports = app;
